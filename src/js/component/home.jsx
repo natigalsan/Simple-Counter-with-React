dd01965a55e2be76820e32fec2a704d1f757e768
natigalsan <natividadgalsan@gmail.com>
2022-05-03 10:27:01 +0000

    comienzo de simple counter(div)

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,27 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
+	//  1. aqui lo que hago es convertir los numeros a texto con  el toString, porque lo recibo en numero y por ello lo paso a texto
+	let numeros = props.segundos.toString();
+	//2. si lo que busco es captar una posicion concreta del string, por ejemplo la unidad hago lo siguiente:
+	// 2.1 para capturar solo la última posicion del numero pasado a string debo hacer:
+	// numeros[numeros.lenght - 1] ; los numero en la ultima posicion del string numero me dará:
+	//3. de esta manera construyo mi simple counter:
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">SIMPLE COUNTER WITH REACT</h1>
+			
+				<div className="reloj">
+					<i class="far fa-clock"></i>
+				</div>
+				<div className="unidad">{numeros[numeros.length - 1]}</div>
+				<div className="decenas">{numeros[numeros.length - 2]}</div>
+				<div className="centenas">
+					{numeros[numeros.length - 3]}
+				</div>
+				<div className="mil">{numeros[numeros.length - 4]}</div>
+			
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,16 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-
+let contador = 0;
+setInterval(function () {
+	//aqui hago que cada un segundo me diga hola, soy natiwai
+	console.log(contador);
+	ReactDOM.render(
+		<Home segundos={contador} />, // en este caso el props de HOMe van a ser los segundos.
+		document.querySelector("#app")
+        
+	);
+    contador += 1;
+}, 500); //quiero que cada segundo que pase quiero renderizar el DOM, por lo tanto debo colocar ReactDOM dentro de la función setInterval.
+//para que mi HOME me renderice el contador necesito utilizar los "props" que en este caso estamos hablando de segundos
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
